780e7295277ee7aa62b3d6b1d48b79511ce4bd81
ayushdama11 <ayushdama11@gmail.com>
2025-07-14 23:04:37 +0530

    revisiting things

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import "./layout.scss"
+import Navbar from "./components/navbar/Navbar" 
+import HomePage from "./routes/homePage/homePage"
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
